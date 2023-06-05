773406d10acf4330ed2f171c0ef2d8ab64f403b0
Konstructa <elenprojects7@gmail.com>
2023-06-05 15:46:32 -0300

    Feat: header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,22 @@
 import P from 'prop-types';
 import * as Styled from './styles';
+import logo from '../../assets/logo-dio.svg'
 
 export const Header = () => {
   return (
     <Styled.Header>
-        
+        <img src={logo} alt='img' />
+        <Styled.Nav>
+          <Styled.NavItem>
+            <p>Home</p>
+          </Styled.NavItem>
+          <Styled.NavButton>
+            <p>Entrar</p>
+          </Styled.NavButton>
+          <Styled.NavButton>
+            <p>Cadastrar</p>
+          </Styled.NavButton>
+        </Styled.Nav>
     </Styled.Header>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,4 +4,32 @@ export const Header = styled.header`
     background-color: #151515;
     width: 100vw;
     height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 10rem;
 `;
+
+export const NavButton = styled.div`
+    padding: 3px 20px;
+    font-weight: 400;
+    font-size: 1.8rem;
+    background-color: #565656;
+    border-radius: 15px;
+    cursor: pointer;
+`;
+
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    min-width: 35rem;
+
+`;
+
+export const NavItem = styled.div`
+    font-weight: 400;
+    font-size: 1.8rem;
+`;
+
+export const Logo = styled.img``;
